f39cb972a808931c265dd9dec45897fd79a54f62
mohiandipta <mohianmostofa@gmail.com>
2021-09-18 12:39:31 +0600

    Add project files.

---
+++ EmployeeRegister/Models/EmployeeDBcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Models
+{
+    public class EmployeeDBcontext : DbContext
+    {
+        public EmployeeDBcontext(DbContextOptions<EmployeeDBcontext>options):base(options)
+        {
+
+        }
+    }
+}
---
+++ EmployeeRegister/Models/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName="nvarchar(50)")]
+        public int EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public int Occupation { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int ImageName { get; set; }
+    }
+}
